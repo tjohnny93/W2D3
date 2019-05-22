2a29fb68b34a79f5f9777984e9e8b157852aceb7
tjohnny93 <tjohnny93@berkeley.edu>
2019-05-22 16:11:01 -0700

    Finish first TDD

--- TDD/lib/tdd.rb
+++ TDD/lib/tdd.rb
@@ -1,5 +1,49 @@
-class Tdd
+require "byebug"
+class Array
+
+  def my_uniq
+    result = []
+    count = Hash.new(0)
+    self.each do |ele|
+      count[ele] += 1
+    end
+
+    count.each do |k, v|
+      result << k
   
+    end
 
+    result 
+  end
 
+  def two_sum
+    results = []
+    self.each_with_index do |el, i|
+      self.each_with_index do |el2, j|
+        if i < j && el + el2 == 0
+          results << [i,j]
+        end
+      end
+    end
+
+    results
+  end
+
+  def my_transpose
+    results = []
+    (0...self.length).each do |row| 
+      new_row = []
+       
+      (0...self.length).each do |col|
+        new_row << self[col][row]
+      end
+
+      results << new_row
+    end
+    
+    results
+    
+  end
+  
+    
 end
--- TDD/spec/tdd_spec.rb
+++ TDD/spec/tdd_spec.rb
@@ -1,11 +1,30 @@
 require 'tdd'
 
-Rspec.describe Tdd do 
+RSpec.describe Array do
   describe "#my_uniq" do
-    it "returns the unique elements in order" do
-      expect([1, 2, 1, 3, 3].uniq).to eq([1, 2, 3]) 
+    it "returns the unique elements in order." do
+      expect([1, 2, 1, 3, 3].my_uniq).to eq([1, 2, 3]) 
+    end
+    it "do not mutate the input array." do
+      expect([1, 2, 1, 3, 3].object_id).to_not eq([1,2,3].object_id)
     end
   end
+
+  describe "#two_sum" do
+    it "finds all pairs of positions where the element at those pos sum to zero." do
+      expect([-1, 0, 2, -2, 1].two_sum).to eq([[0,4], [2,3]])
+    end
+  end
+
+  describe "#my_transpose" do
+    it "convert between the row-oriented and column-oriented representations." do
+      expect([[0, 1, 2],[3, 4, 5],[6, 7, 8]].my_transpose).to eq([[0, 3, 6],[1, 4, 7],[ 2, 5, 8]])
+    end
+    it "does NOT call the built in Array#transpose" do
+      expect([[0, 1, 2],[3, 4, 5],[6, 7, 8]].my_transpose).not_to receive(:transpose)
+    end
+  end
+      
 end
       
 
